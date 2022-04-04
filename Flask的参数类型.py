4cc0305cc14e650d8e13d97a33fee669dc3cffb7
董文宇 <82132620+ddd887@users.noreply.github.com>
2022-04-04 14:59:26 +0800

    Add files via upload

---
+++ Flask的参数类型.py
@@ -0,0 +1,25 @@
+import flask
+from flask import *
+
+#创建flask程序
+app = Flask(__name__,
+            static_url_path='/static',#静态文件路径
+            static_folder='static',
+            template_folder='templages' #模板文件
+            )
+
+@app.route('/abc',method=['GET','POST'])
+def a_page():
+    user_name = request.form.get('uname')
+    user_pass = request.form.get('upass')
+    return "this is a page %s hahaha"%(request.headers.get('User-agent'))
+    pass
+
+#装饰器,关联路由
+@app.route('/')
+def index():
+    return "haha"
+    pass
+
+if __name__ == '__main__':
+    app.run()
