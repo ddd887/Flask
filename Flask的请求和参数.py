4c6cc6204e675cedc6e9f695a965a0b332fd7abc
董文宇 <82132620+ddd887@users.noreply.github.com>
2022-04-04 14:53:06 +0800

    Update Flask的请求和参数.py

--- Flask的请求和参数.py
+++ Flask的请求和参数.py
@@ -8,10 +8,26 @@ app = Flask(__name__,
             template_folder='templages' #模板文件
             )
 
+@app.route('/ac/<user>/<passwd>')
+def c_page(user,passwd):
+    return "this is a page %s ----- %s hahaha"%(user,passwd)       
+            
+
+
+
+#get请求传参
 @app.route('/abc',method=['GET'])
 def a_page():
     user_name = request.args.get('uname')
     user_pass = request.args.get('upass')
+    return "this is a page %s --- %s hahaha"%(user_name,user_pass)
+    pass
+
+#post请求传参
+@app.route('/ab',method=['POST'])
+def b_page():
+    user_name = request.form.get('uname')
+    user_pass = request.form.get('upass')
     return "this is a page %s --- %s hahaha"%(user_name,user_pass)
     pass
 
