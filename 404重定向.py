eda27b7691491b1266547c86f7dcee93ef2c6952
董文宇 <82132620+ddd887@users.noreply.github.com>
2022-04-04 16:32:14 +0800

    Add files via upload

---
+++ 404重定向.py
@@ -0,0 +1,30 @@
+import flask
+from flask import *
+
+#创建flask程序
+app = Flask(__name__,
+            static_url_path='/static',#静态文件路径
+            static_folder='static',
+            template_folder='templages' #模板文件
+            )
+
+#404重定向
+@app.errorhandler(404)
+def page_not_found(e):
+    return '你出错了',404
+    pass
+
+@app.route('/a')
+def a_page():
+    return 'xxx'
+    pass
+
+
+#装饰器,关联路由
+@app.route('/')
+def index():
+    return "haha"
+    pass
+
+if __name__ == '__main__':
+    app.run()
