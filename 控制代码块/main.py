5e6af306b0df83962e76b0a8ee6b59f156b3da56
董文宇 <82132620+ddd887@users.noreply.github.com>
2022-04-05 16:03:30 +0800

    Create main.py

---
+++ 控制代码块/main.py
@@ -0,0 +1,34 @@
+import flask
+from flask import *
+
+#创建flask程序
+app = Flask(__name__,
+            static_url_path='/static',#静态文件路径
+            static_folder='static',
+            template_folder='templages' #模板文件
+            )
+
+@app.route('/a/<id>')
+def a_page(id):
+    #python参数进来都是string类型
+    m_int = int(id)
+    m_str = "haha nihao"
+    m_list = ["xiaoming","xiaohong","xiaoli"]
+    vip = 0
+    return flask.render_template("tempa.html",mint=m_int,mstr=m_str,mlist=m_list,vip=vip)
+    pass
+
+#装饰器,关联路由
+@app.route('/')
+def index():
+    return "haha"
+    pass
+
+#404重定向
+@app.errorhandler(404)
+def page_not_found(e):
+    return '你出错了',404
+    pass
+
+if __name__ == '__main__':
+    app.run()
